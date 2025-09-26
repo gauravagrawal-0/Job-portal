e9eb84c919197b8fdac764cf42a60296d427b888
Gaurav Agrawal <gauravagrawal855@gmail.com>
2025-09-27 00:11:12 +0530

    Some modifications on landing page and create a new file called hero

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 const LandingPage = () => {
   return (
@@ -7,6 +8,7 @@ const LandingPage = () => {
    
     <div>
        <Header/>
+       <Hero /> 
     </div>
   )
 }
